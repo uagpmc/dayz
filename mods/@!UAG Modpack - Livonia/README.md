e6727e852d49177e0099835581f526ebfc5bcbcf
Alex <zuedev@gmail.com>
2023-04-11 10:03:42 +0100

    initial upload

---
+++ mods/@!UAG Modpack - Livonia/README.md
@@ -0,0 +1,5 @@
+# UAG Modpack - Livonia
+
+Load this when you want to play on the Unnamed Group DayZ Livonia server.
+
+All of our mods are open source! Check them out here: https://github.com/uagpmc/dayz
---
+++ mods/@!UAG Modpack - Livonia/addons/uag_modpack_livonia/config.cpp
@@ -0,0 +1,11 @@
+class CfgPatches {
+    class uag_modpack_livonia {
+        authors[] = {"uagpmc","zuedev"};
+        authorUrl = "https://uagpmc.com";
+        name = "uag_modpack_livonia";
+        requiredAddons[] = {};
+        requiredVersion = 1.20;
+        units[] = {};
+        weapons[] = {};
+    }
+}
