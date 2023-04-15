e409f6704317c26c0f0258ad1637083d34547b18
Alex <zuedev@gmail.com>
2023-04-15 15:46:05 +0100

    add chernarus setup mod

---
+++ mods/@!UAG Modpack - Chernarus/README.md
@@ -0,0 +1,5 @@
+# UAG Modpack - Chernarus
+
+Load this when you want to play on the Unnamed Group DayZ Chernarus server.
+
+All of our mods are open source! Check them out here: https://github.com/uagpmc/dayz
---
+++ mods/@!UAG Modpack - Chernarus/addons/uag_modpack_chernarus/config.cpp
@@ -0,0 +1,11 @@
+class CfgPatches {
+    class uag_modpack_chernarus {
+        authors[] = {"uagpmc","zuedev"};
+        authorUrl = "https://uagpmc.com";
+        name = "uag_modpack_chernarus";
+        requiredAddons[] = {};
+        requiredVersion = 1.20;
+        units[] = {};
+        weapons[] = {};
+    }
+}
